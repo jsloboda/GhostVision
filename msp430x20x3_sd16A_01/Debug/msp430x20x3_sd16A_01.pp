b56532e19bfb7a86c3fef2ff847edd031dd1886c
John Lee <leedhyun@umich.edu>
2014-03-08 18:54:05 -0500

    MSP430F2013 ADC Testing
    This project is for testing the 16 bit sigma-delta ADC.

---
+++ msp430x20x3_sd16A_01/Debug/msp430x20x3_sd16A_01.pp
@@ -0,0 +1,13 @@
+# FIXED
+
+msp430x20x3_sd16A_01.obj: ../msp430x20x3_sd16A_01.c
+msp430x20x3_sd16A_01.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
+msp430x20x3_sd16A_01.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430f2013.h
+msp430x20x3_sd16A_01.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+msp430x20x3_sd16A_01.obj: C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+
+../msp430x20x3_sd16A_01.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430f2013.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
---
+++ msp430x20x3_sd16A_01/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f2013.cmd 
+
+C_SRCS += \
+../msp430x20x3_sd16A_01.c 
+
+OBJS += \
+./msp430x20x3_sd16A_01.obj 
+
+C_DEPS += \
+./msp430x20x3_sd16A_01.pp 
+
+C_DEPS__QUOTED += \
+"msp430x20x3_sd16A_01.pp" 
+
+OBJS__QUOTED += \
+"msp430x20x3_sd16A_01.obj" 
+
+C_SRCS__QUOTED += \
+"../msp430x20x3_sd16A_01.c" 
+
+
